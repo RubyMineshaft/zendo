84446501eada71df1e4221e81971370b50da1ea0
mattetress <mattetress@me.com>
2020-09-17 13:18:47 -0500

    add buttons to change size/color

--- graphics.js
+++ graphics.js
@@ -1,20 +1,59 @@
+
+let size = 7;
+let board = new Board(size, size);
+const R = 20;
+const D = 2 * R;
+  let width = board.width * D + D;
+  let height = board.height * D + D;
+
 function setup() {
-    placeColor = 1
-    bg = "gray"
-    R = 20
-    D = 2 * R
-    board = new Board(7, 7)
-    width = board.width * D + D
-    height = board.height * D + D
+  placeColor = 1
+  bg = "gray"
+
     createCanvas(width, height).mouseClicked(place)
     ellipseMode(RADIUS)
     strokeCap(PROJECT)
     textSize(25)
     textAlign(CENTER, CENTER)
-    button = createButton('Save as image')
-    button.position(19, 19)
-    button.mousePressed(saveCanvas)
+
+    saveButton = createButton('Save as image');
+    saveButton.position(19, 19);
+    saveButton.mousePressed(saveCanvas);
+
+    fiveFive = createButton('5 x 5');
+    fiveFive.position(19,50);
+    fiveFive.mousePressed(resizeBoard5);
+
+    sevenSeven = createButton('7 x 7');
+    sevenSeven.position(19, 75);
+    sevenSeven.mousePressed(resizeBoard7);
+
+    nineNine = createButton('9 x 9');
+    nineNine.position(19, 100);
+    nineNine.mousePressed(resizeBoard9);
+
+    buttonGreen = createButton("Green");
+    buttonGreen.position(19, 150);
+    buttonGreen.mousePressed(setGreen);
+
+    buttonRed = createButton("Red");
+    buttonRed.position(19, 175);
+    buttonRed.mousePressed(setRed);
+
+    buttonGray = createButton("Gray");
+    buttonGray.position(19, 200);
+    buttonGray.mousePressed(setGray);
 }
+
+function setSize(newSize) {
+  size = newSize;
+  board = new Board(size, size);
+  width = board.width * D + D;
+  height = board.height * D + D;
+  resizeCanvas((size * D + D), (size * D + D));
+}
+
+
 
 function draw() {
     background(bg)
@@ -44,8 +83,8 @@ function draw() {
         }
     }
 
-    let x = floor(map(mouseX, R, width - R, 0, board.width, true))
-    let y = floor(map(mouseY, R, height - R, 0, board.height, true))
+    let x = floor(map(mouseX, R, width - R, 0, width, true))
+    let y = floor(map(mouseY, R, height - R, 0, height, true))
     if (board[x] && board[x][y]) {
         let node = board[x][y]
         if (keyIsDown(90)) node.color = 1
@@ -66,9 +105,33 @@ function place() {
 }
 
 function keyPressed() {
-    if (key == 'q') bg = 'gray'
-    if (key == 'w') bg = 'lime'
-    if (key == 'e') bg = 'red'
+    if (key == 'q') setGray();
+    if (key == 'w') setGreen();
+    if (key == 'e') setRed();
 
-    if (key == 's') saveCanvas()
+    if (key == 's') saveCanvas();
+}
+
+function resizeBoard5() {
+  setSize(5);
+}
+
+function resizeBoard7() {
+  setSize(7);
+}
+
+function resizeBoard9() {
+  setSize(9);
+}
+
+function setGreen() {
+  bg = 'lime';
+}
+
+function setRed() {
+  bg = "red";
+}
+
+function setGray() {
+  bg = "gray";
 }
